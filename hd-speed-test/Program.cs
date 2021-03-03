37d588f2de2138f40a41262b2e1de7affc4d45b3
Avishkar Autar <aautar@digital-radiation.com>
2021-03-02 19:16:29 -0500

    Add project files.

---
+++ hd-speed-test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hd_speed_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
